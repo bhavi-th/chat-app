d4ed00b11aa337d20ab6beae195de56303b56c4b
bhavi-th <bhavith15@gmail.com>
2025-08-15 23:41:15 +0530

    Initialized frontend and backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (res, req) => {
+    res.send("signup route")
+}
+
+export const login = (res, req) => {
+    res.send("login route")
+}
+
+export const logout = (res, req) => {
+    res.send("logout route")
+}
